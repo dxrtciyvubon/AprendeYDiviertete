5141023b78c2db0dbc7ed74dca4445db8a3c6ff6
HugoCanulPoot <49693926+HugoCanulPoot@users.noreply.github.com>
2019-06-09 17:43:35 -0500

    frutasyverduras

--- app/src/main/java/com/example/kidsapp/MainActivity.java
+++ app/src/main/java/com/example/kidsapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kidsapp;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void frutas(View view) {
+        Intent uno= new Intent (this,frutas.class);
+        startActivity(uno);
+    }
+
+    public void verduras(View view) {
+        Intent uno= new Intent (this,verduras.class);
+        startActivity(uno);
+    }
+
+
+    }
+
